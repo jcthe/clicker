2b6a64ce5abbeb39289c3868df939e182b8b7446
8226 <8226@fresnou.org>
2023-04-13 10:28:35 -0700

    i have commit

---
+++ MadLibs.js
@@ -0,0 +1,29 @@
+const readLine=require ('readline-sync');
+let question1 = readLine.question("Enter Noun");
+let question2 = readLine.question("Enter Noun");
+let question3 = readLine.question("Enter Noun");
+let question4 = readLine.question("Enter Noun");
+let question5 = readLine.question("Enter Noun");
+let question6 = readLine.question("Enter Noun");
+let question7 = readLine.question("Enter Noun");
+
+let question8  = readLine.question("Enter adjective");
+let question9  = readLine.question("Enter adjective");
+let question10 = readLine.question("Enter adjective");
+let question11 = readLine.question("Enter adjective");
+let question12 = readLine.question("Enter adjective");
+let question13 = readLine.question("Enter adjective");
+let question14 = readLine.question("Enter adjective");
+ 
+let question15 = readLine.question("Enter verb");
+let question16 = readLine.question("Enter verb");
+let question17 = readLine.question("Enter verb");
+let question18 = readLine.question("Enter verb");
+let question19 = readLine.question("Enter verb");
+let question20 = readLine.question("Enter verb");
+let question21 = readLine.question("Enter verb");
+
+let myList = [];
+myList.push('1');
+myList.push('2');
+myList.push(readLine.question('Enter a noun'));
