1db72086a1de21474ae07b28f805360bc713cbd3
8226 <8226@fresnou.org>
2023-04-27 09:54:09 -0700

    I have commited

---
+++ 4.5.3.js
@@ -0,0 +1,44 @@
+const readLine = require('readline-sync');
+
+
+let sandwich = readLine.question("What type of sandwich would you like(chicken, beef, or tofu):");
+
+let sandwhichCost = readLine.question("that would be $5.25");
+
+let sandwichCost = {
+    chicken: 5.25,
+    beef: 6.25,
+    tofu: 6.75,
+};
+
+
+
+
+
+let beverage = readLine.question("would like a drink with that");
+
+let drink = readLine.question("What size of a drink would you like");
+
+let drinkCost = console.log("That will be $1.00");
+
+
+
+console.log("so far your total is $6.28");
+
+
+
+
+let frenchfries = readLine.question("Would you like some fries with that");
+
+let frenchfriesCost =readLine.question("Small is $1.00, medium is 1.50, large is 2.00, [so which size would you like");
+console.log(" Your total cost is $7.28");
+
+
+
+
+myOrder={
+    chicken:5.25,
+    small: 1.00,
+    small: 1.00,
+    totalCost: 7.28,
+};
